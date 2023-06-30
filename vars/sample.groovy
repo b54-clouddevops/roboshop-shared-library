455d34f534a60cbcc4bae815b93edbb14c0a585e
CodingManoj <mvp2612@hotmail.com>
2023-06-29 21:54:00 -0400

    Auto Push

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,7 +1,8 @@
 // declaring a function info 
-def info() {
-    echo "Hai, I am function who name is info"
+def info(message) {
+    echo "Hai, I am function whose name is info"
+    echo "I am printing the value of message is $message"
 }
 
 // calling the function info 
-info()
+info("ValueOfMessage")
