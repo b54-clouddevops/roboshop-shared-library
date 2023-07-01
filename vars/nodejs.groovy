f5b956b7936837ea12f324ddcfd522e93ca342b5
CodingManoj <mvp2612@hotmail.com>
2023-06-30 20:56:07 -0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,8 +1,8 @@
 def lintChecks(){
     sh ''' 
-        sh "echo Installing JSLint"
-        sh "npm i jslint"
-        sh "node_modules/jslint/bin/jslint.js server.js || true"
+        echo Installing JSLint
+        npm i jslint
+        node_modules/jslint/bin/jslint.js server.js || true
 
     ''' 
 }
