cced5678fe2bf8b3cf8fb892d8d0645b0d10536a
CodingManoj <mvp2612@hotmail.com>
2023-07-03 21:06:39 -0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -8,6 +8,15 @@ def lintChecks(){
     ''' 
 }
 
+def sonarChecks(){
+    sh ''' 
+        echo Sonar Checks In Progress
+        sonar-scanner -Dsonar.sources=. -Dsonar.login=1cbe0f6f0cdbd9bb3ff8d78b880f7fb417eb4321  -Dsonar.host.url=http://172.31.86.248:9000 -Dsonar.projectKey=${COMPONENT}
+        echo Sonar Checks Completed
+
+    ''' 
+}
+
 
 def call(COMPONENT) {
     pipeline {
@@ -25,6 +34,14 @@ def call(COMPONENT) {
             stage('Code Compile') {
                 steps {                
                         sh "npm install"
+                }
+            }
+
+            stage('Sonar Checks') {
+                steps {
+                    script {                
+                        sonarChecks()
+                    }
                 }
             }
 
