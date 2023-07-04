c4c3e89784eec2d1b25a2424adf25fc26d814cf7
CodingManoj <mvp2612@hotmail.com>
2023-07-03 21:25:59 -0400

    Auto Push

---
+++ common.groovy
@@ -0,0 +1,10 @@
+def sonarChecks(){
+    sh ''' 
+        echo Sonar Checks In Progress
+        sonar-scanner -Dsonar.sources=. -Dsonar.login=1cbe0f6f0cdbd9bb3ff8d78b880f7fb417eb4321  -Dsonar.host.url=http://172.31.86.248:9000 -Dsonar.projectKey=${COMPONENT}
+        curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh
+        sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONATURL} ${COMPONENT}
+        echo Sonar Checks Completed
+
+    ''' 
+}
--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,18 +4,6 @@ def lintChecks(){
         npm i jslint
         node_modules/jslint/bin/jslint.js server.js || true
         echo lint checks completed for ${COMPONENT}
-
-    ''' 
-}
-
-def sonarChecks(){
-    sh ''' 
-        echo Sonar Checks In Progress
-        sonar-scanner -Dsonar.sources=. -Dsonar.login=1cbe0f6f0cdbd9bb3ff8d78b880f7fb417eb4321  -Dsonar.host.url=http://172.31.86.248:9000 -Dsonar.projectKey=${COMPONENT}
-        curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh
-        sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONATURL} ${COMPONENT}
-        echo Sonar Checks Completed
-
     ''' 
 }
 
@@ -46,7 +34,7 @@ def call(COMPONENT) {
             stage('Sonar Checks') {
                 steps {
                     script {                
-                        sonarChecks()
+                        common.sonarChecks()
                     }
                 }
             }
