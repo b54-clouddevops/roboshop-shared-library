7651385cb2cd80ed1665d3267c08a84c0b940118
CodingManoj <mvp2612@hotmail.com>
2023-07-03 21:50:06 -0400

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def sonarChecks(){
     sh ''' 
         echo Sonar Checks In Progress
-        sonar-scanner -Dsonar.login="${SONARCRED_USR}" -Dsonar.password="${SONARCRED_PSW}"  -Dsonar.host.url=http://172.31.86.248:9000 -Dsonar.projectKey=${COMPONENT} ${ARGS}
+        sonar-scanner -Dsonar.login=${SONARCRED_USR} -Dsonar.password=${SONARCRED_PSW}  -Dsonar.host.url=http://172.31.86.248:9000 -Dsonar.projectKey=${COMPONENT} ${ARGS}
         curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > sonar-quality-gate.sh
         sonar-quality-gate.sh ${SONARCRED_USR} ${SONARCRED_PSW} ${SONATURL} ${COMPONENT}
         echo Sonar Checks Completed
