5a9771a45191ada1eb9a2ec6ed529072430c4441
CodingManoj <mvp2612@hotmail.com>
2023-07-06 21:30:32 -0400

    Auto Push

--- vars/angularjs.groovy
+++ vars/angularjs.groovy
@@ -1,38 +1,44 @@
-def lintChecks(){
-    sh ''' 
-        echo Installing AngularLint for ${COMPONENT}
-        echo lint checks completed for ${COMPONENT}
-
-    ''' 
-}
-
-
-def call(COMPONENT) {
-    pipeline {
-        agent {  label 'WS' }
-        environment {
-            SONARCRED = credentials('SONARCRED') 
-            SONATURL  = "172.31.86.248"
-        }
-        stages {      
-
-            stage('Lint Checks') {
-                steps { 
-                    script {
-                        lintChecks()
-                    }
-                }
-            }
-
-            stage('Code Quality Analysis') {
-                steps {                
-                    script {
-                        env.ARGS="-Dsonar.sources=."              
-                        common.sonarChecks()
-                    }
-                }
-            }
-
-        }                                                                             
+def call() {
+    node {
+        common.lintChecks()
     }
 }
+
+// def lintChecks(){
+//     sh ''' 
+//         echo Installing AngularLint for ${COMPONENT}
+//         echo lint checks completed for ${COMPONENT}
+
+//     ''' 
+// }
+
+
+// def call(COMPONENT) {
+//     pipeline {
+//         agent {  label 'WS' }
+//         environment {
+//             SONARCRED = credentials('SONARCRED') 
+//             SONATURL  = "172.31.86.248"
+//         }
+//         stages {      
+
+//             stage('Lint Checks') {
+//                 steps { 
+//                     script {
+//                         lintChecks()
+//                     }
+//                 }
+//             }
+
+//             stage('Code Quality Analysis') {
+//                 steps {                
+//                     script {
+//                         env.ARGS="-Dsonar.sources=."              
+//                         common.sonarChecks()
+//                     }
+//                 }
+//             }
+
+//         }                                                                             
+//     }
+// }
