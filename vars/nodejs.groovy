24f7ee51a299eb5153e33d55cf3abfd51f0674c1
CodingManoj <mvp2612@hotmail.com>
2023-07-06 20:27:52 -0400

    Auto Push

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -69,7 +69,7 @@ def call(COMPONENT) {
                 when { expression { env.TAG_NAME != null } }
                 steps {
                     script {
-                        env.UPLOAD_STATUS=sh (returnStdout: true, script: 'curl -s -L http://${NEXUSURL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}')
+                       env.UPLOAD_STATUS=sh (returnStdout: true, script: 'curl -L -s http://${NEXUSURL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip')
                     }
                     sh "echo Upload Status Is ${UPLOAD_STATUS}"
                 }
