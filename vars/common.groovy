f3dbd26883214d206574111204f328e2e79db6bb
CodingManoj <mvp2612@hotmail.com>
2023-07-07 20:40:30 -0400

    Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -104,13 +104,12 @@ def artifacts() {
                 
                 stage('Upload Artifacts') {
                         if(env.APP_TYPE == "nodejs"){
-                                sh '''
-                                        echo Uploading ${COMPONENT} Artifacts To Nexus
-                                        curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.92.189:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-                                        echo Uploading ${COMPONENT} Artifacts To Nexus is Completed                   
-                                '''
-                        }
-                
+                                withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
+                                        sh "echo Uploading ${COMPONENT} Artifacts To Nexus"
+                                        sh "curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip  http://172.31.92.189:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+                                        sh "echo Uploading ${COMPONENT} Artifacts To Nexus is Completed"                   
+                                }
+                        }                
                 }
         }
 }
