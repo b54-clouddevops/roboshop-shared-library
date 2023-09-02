468352781c6fca39ef538e3a1e2fb06ad670e9d9
CodingManoj <mvp2612@hotmail.com>
2023-09-01 20:57:41 -0400

    Git Auto Push

---
+++ vars/docker.groovy
@@ -0,0 +1,45 @@
+def call() {
+    node {
+        sh "rm -rf *"
+        git branch: 'main', url: "https://github.com/b54-clouddevops/${COMPONENT}.git"
+        env.APP_TYPE="nodejs"
+        common.lintChecks()
+        if(env.TAG_NAME != null) {
+                stage('Prepare Artifacts') {
+                        if(env.APP_TYPE == "nodejs"){
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        npm install                     
+                                '''
+                        }
+                        else if(env.APP_TYPE == "python"){
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        zip -r ${COMPONENT}-${TAG_NAME}.zip *.py *.ini  requirements.txt                     
+                                '''
+                        }
+                        else if(env.APP_TYPE == "java"){
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        mvn clean package
+                                        ls -ltr         
+                                '''
+                        }
+                        else {
+                                sh '''
+                                        echo Preparing Artifacts for ${COMPONENT}
+                                        cd static
+                                        zip -r ../${COMPONENT}-${TAG_NAME}.zip *                  
+                                '''
+                        }
+
+                        sh "env"
+                        sh "docker build -t 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest ."
+                        sh "docker tag 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
+                        sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 355449129696.dkr.ecr.us-east-1.amazonaws.com"
+                        sh "355449129696.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}"
+                    
+                    }
+                }
+            }
+        }
